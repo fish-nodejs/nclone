4f1cd2f6021cce7ebc9baebd0ec7dd41834764f7
apple <bitfishxyz@gmail.com>
2019-02-13 19:38:10 +0800

    use axios and it works

--- test/downloadFolder.js
+++ test/downloadFolder.js
@@ -1,5 +1,5 @@
 const path = require('path')
 let downloadFolder = require('../lib/downloadFolder')
 
-let url = 'https://api.github.com/repos/saltfish666/nclone/contents'
+let url = 'https://api.github.com/repos/vuejs/vue/contents'
 downloadFolder(url, path.join(__dirname, './test'), 5)
--- test/getFolderInfo.js
+++ test/getFolderInfo.js
@@ -1,6 +1,8 @@
 const getFloderInfo = require('../lib/getFolderInfo')
 
+console.log(11)
 getFloderInfo('https://api.github.com/repos/saltfish666/nclone/contents', (err, res) => {
   if (err) throw err
   console.log(res)
+  console.log(2)
 })
